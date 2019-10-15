be98886c05c363870a7562173e319c251fcad94a
w.slot <wesselslot1@gmail.com>
2019-10-16 00:25:19 +0200

    test

--- wp-content/themes/laurainesdogcare/shared-templates/footer.php
+++ wp-content/themes/laurainesdogcare/shared-templates/footer.php
@@ -8,7 +8,9 @@ $core = new Core();
             <div class="row">
                 <a target="" href="https://www.instagram.com/lauraine_dogcare/">
                     <div id="instagram">
-                        <?php $data = $core->httpHelper->getRequest('https://www.instagram.com/lauraine_dogcare/?__a=1');
+                        <?php $data = $core->httpHelper->getRequest('https://www.instagram.com/lauraine_dogcare/?__a=1');\
+
+                        var_dump($data);
                         foreach ($data->graphql->user->edge_owner_to_timeline_media->edges as $item) { ?>
                             <div class="insta-item">
                                 <img src="<?php echo $item->node->thumbnail_src ?>" alt="">
