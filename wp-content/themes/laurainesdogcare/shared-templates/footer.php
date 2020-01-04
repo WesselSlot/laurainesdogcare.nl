d73fa168823e1739bca0856c6b6bc744d04bbfc5
w.slot <wesselslot1@gmail.com>
2020-01-04 19:45:51 +0100

    fix footer

--- wp-content/themes/laurainesdogcare/shared-templates/footer.php
+++ wp-content/themes/laurainesdogcare/shared-templates/footer.php
@@ -1,38 +1,5 @@
-<?php
-    function getRequest($url) {
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        $response = curl_exec($ch);
-        return json_decode($response);
-
-    }
-?>
 <?php wp_footer(); ?>
     <footer>
-        <div class="container-fluid">
-            <div class="row">
-                <a target="" href="https://www.instagram.com/lauraine_dogcare/">
-                    <div id="instagram">
-                        <?php
-                        $ch = curl_init();
-                        curl_setopt($ch, CURLOPT_URL, "https://www.google.nl/");
-                        // Set so curl_exec returns the result instead of outputting it.
-                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                        // Get the response and close the channel.
-                        $response = curl_exec($ch);
-
-                        curl_close($ch);
-                        var_dump($response);
-                        foreach ($response->graphql->user->edge_owner_to_timeline_media->edges as $item) { ?>
-                            <div class="insta-item">
-                                <img src="<?php echo $item->node->thumbnail_src ?>" alt="">
-                            </div>
-                        <?php } ?>
-                    </div>
-                </a>
-            </div>
-        </div>
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-md-10 mx-auto">
