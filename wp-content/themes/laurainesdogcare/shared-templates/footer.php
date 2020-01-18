924cecf44e7e880ec65a2a34a6decf56184fb89d
w.slot <wesselslot1@gmail.com>
2020-01-18 19:39:54 +0100

    Alle

--- wp-content/themes/laurainesdogcare/shared-templates/footer.php
+++ wp-content/themes/laurainesdogcare/shared-templates/footer.php
@@ -3,10 +3,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-md-10 mx-auto">
-                    <p class="copyright text-muted">
+                    <p class="copyright text-muted">Copyright &copy; <?php bloginfo('name');?> <?php echo DATE("Y")?></p>
+                    <p class="copyright text-muted links">
                         <a class="text-muted" href="">Algemene voorwaarde</a>
                     </p>
-                    <p class="copyright text-muted">Copyright &copy; <?php bloginfo('name');?> <?php echo DATE("Y")?></p>
                 </div>
             </div>
         </div>
