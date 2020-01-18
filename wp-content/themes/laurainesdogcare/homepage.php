075d937871c1cd3e6ee53eda2a0517e6999390a9
w.slot <wesselslot1@gmail.com>
2020-01-18 13:51:59 +0100

    Add the images and fix some style

--- wp-content/themes/laurainesdogcare/homepage.php
+++ wp-content/themes/laurainesdogcare/homepage.php
@@ -6,7 +6,7 @@
 <?php $metaData = get_post_custom(get_the_ID());
 ?>
 
-<header class="masthead" style="background-image: url(<?php echo get_template_directory_uri().'/images/home-bg.jpg' ?>)">
+<header class="masthead" style="background-image: url(<?php echo get_template_directory_uri().'/images/ed7ac935-ad13-4a6b-aa22-b97cb1457fa1.jpg' ?>)">
     <div class="overlay"></div>
     <div class="container">
         <div class="row">
@@ -35,25 +35,25 @@
 
 <div class="container-fluid background-color-gray">
     <div class="container">
-            <div id="workflow">
-                <h3 class="font-color-primary">Tevreden klanten</h3>
+            <div id="reviews">
+                <h3 class="font-color-primary">Tevreden honden</h3>
                 <div class="card-deck">
                     <div class="card">
-                        <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22277%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20277%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16dd0b3ff9b%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16dd0b3ff9b%22%3E%3Crect%20width%3D%22277%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22102.6875%22%20y%3D%22106.24375%22%3E277x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap">
+                        <img class="card-img-top" src="<?php echo get_template_directory_uri().'/images/IMG_3528.jpg' ?>" alt="Card image cap">
                         <div class="card-body">
                             <h5 class="card-title">Hond 1</h5>
                             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         </div>
                     </div>
                     <div class="card">
-                        <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22277%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20277%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16dd0b3ff9b%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16dd0b3ff9b%22%3E%3Crect%20width%3D%22277%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22102.6875%22%20y%3D%22106.24375%22%3E277x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap">
+                        <img class="card-img-top" src="<?php echo get_template_directory_uri().'/images/IMG_5430.jpg' ?>" alt="Card image cap">
                         <div class="card-body">
                             <h5 class="card-title">Hond 2</h5>
                             <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
                         </div>
                     </div>
                     <div class="card">
-                        <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22277%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20277%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16dd0b3ff9b%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16dd0b3ff9b%22%3E%3Crect%20width%3D%22277%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22102.6875%22%20y%3D%22106.24375%22%3E277x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap">
+                        <img class="card-img-top" src="<?php echo get_template_directory_uri().'/images/B8C68543-CF8C-487C-96BA-C88B48EAABC1.jpg' ?>"  alt="Card image cap">
                         <div class="card-body">
                             <h5 class="card-title">Hond 3</h5>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
