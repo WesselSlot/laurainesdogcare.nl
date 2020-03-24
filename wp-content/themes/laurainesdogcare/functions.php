f970c01788b323f3fcc7946ebf61bc6d8460e89e
w.slot <wesselslot1@gmail.com>
2020-03-24 21:59:04 +0100

    test

--- wp-content/themes/laurainesdogcare/functions.php
+++ wp-content/themes/laurainesdogcare/functions.php
@@ -29,18 +29,19 @@ add_theme_support( 'post-thumbnails', array( 'post', 'page' ) );
 add_action("wp_ajax_nopriv_send_form_action", "send_form");
 
 function send_form() {
-    $data = $_POST['data'];
-    $name = trimCharacters($data['name']);
-    $email = trimCharacters($data['email']);
-    $phoneNumber = trimCharacters($data['phone']);
-    $messages = trimCharacters($data['messages']);
-
-    $to = 'wesselslot1@gmail.com';
-    $subject = 'The subject';
-    $body = '<ul><li>' . $name . '</li><li>' . $email . '</li><li>' . $phoneNumber . '</li><li>' . $messages . '</li></ul>';
-    $headers = array('Content-Type: text/html; charset=UTF-8');
-
-    wp_mail($to, $subject, $body, $headers);
+    echo "test";
+//    $data = $_POST['data'];
+//    $name = trimCharacters($data['name']);
+//    $email = trimCharacters($data['email']);
+//    $phoneNumber = trimCharacters($data['phone']);
+//    $messages = trimCharacters($data['messages']);
+//
+//    $to = 'wesselslot1@gmail.com';
+//    $subject = 'The subject';
+//    $body = '<ul><li>' . $name . '</li><li>' . $email . '</li><li>' . $phoneNumber . '</li><li>' . $messages . '</li></ul>';
+//    $headers = array('Content-Type: text/html; charset=UTF-8');
+//
+//    wp_mail($to, $subject, $body, $headers);
 
     wp_die();
 }
--- wp-content/themes/laurainesdogcare/js/mail.js
+++ wp-content/themes/laurainesdogcare/js/mail.js
@@ -1,7 +1,7 @@
 jQuery("Body").ready(function ($) {
     $("#contactForm").submit(function (e) {
         e.preventDefault();
-        var data = [$('#name').val(), $('#email').val(), $('#phone').val(), $('#message').val()];
+        var data = Array($('#name').val(), $('#email').val(), $('#phone').val(), $('#message').val());
         $.post( ajax.url, {
             action: "send_form_action",
             data: data
