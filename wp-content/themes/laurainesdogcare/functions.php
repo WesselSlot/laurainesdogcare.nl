1fed9f854a429ff7f5c4818d79553053e17ce77a
w.slot <wesselslot1@gmail.com>
2020-03-24 21:56:19 +0100

    change functions php

--- wp-content/themes/laurainesdogcare/functions.php
+++ wp-content/themes/laurainesdogcare/functions.php
@@ -26,6 +26,27 @@ add_action( 'init', 'wpb_custom_new_menu' );
 // activate thumbnails
 add_theme_support( 'post-thumbnails', array( 'post', 'page' ) );
 
-include (__DIR__  . '/core/mail.php');
+add_action("wp_ajax_nopriv_send_form_action", "send_form");
+
+function send_form() {
+    $data = $_POST['data'];
+    $name = trimCharacters($data['name']);
+    $email = trimCharacters($data['email']);
+    $phoneNumber = trimCharacters($data['phone']);
+    $messages = trimCharacters($data['messages']);
+
+    $to = 'wesselslot1@gmail.com';
+    $subject = 'The subject';
+    $body = '<ul><li>' . $name . '</li><li>' . $email . '</li><li>' . $phoneNumber . '</li><li>' . $messages . '</li></ul>';
+    $headers = array('Content-Type: text/html; charset=UTF-8');
+
+    wp_mail($to, $subject, $body, $headers);
+
+    wp_die();
+}
+
+function trimCharacters($value) {
+    return htmlspecialchars(trim($value));
+}
 
 
