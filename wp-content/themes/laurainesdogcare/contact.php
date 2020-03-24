93c41e6b695d604e36bb2a0cdc833d9e74fdf225
w.slot <wesselslot1@gmail.com>
2020-03-24 22:21:22 +0100

    temp remove form

--- wp-content/themes/laurainesdogcare/contact.php
+++ wp-content/themes/laurainesdogcare/contact.php
@@ -31,41 +31,41 @@
                 wp_reset_query();
                 ?>
             </p>
-            <form name="sentMessage" method="POST" action="" id="contactForm" novalidate>
-                <div class="control-group">
-                    <div class="form-group floating-label-form-group controls">
-                        <label>Naam</label>
-                        <input type="text" class="form-control" placeholder="Naam" id="name" required data-validation-required-message="Please enter your name.">
-                        <p class="help-block text-danger"></p>
-                    </div>
-                </div>
-                <div class="control-group">
-                    <div class="form-group floating-label-form-group controls">
-                        <label>Email</label>
-                        <input type="email" class="form-control" placeholder="Email" id="email" required data-validation-required-message="Please enter your email address.">
-                        <p class="help-block text-danger"></p>
-                    </div>
-                </div>
-                <div class="control-group">
-                    <div class="form-group col-xs-12 floating-label-form-group controls">
-                        <label>Telefoonnummer</label>
-                        <input type="tel" class="form-control" placeholder="Telefoonnummer" id="phone" required data-validation-required-message="Please enter your phone number.">
-                        <p class="help-block text-danger"></p>
-                    </div>
-                </div>
-                <div class="control-group">
-                    <div class="form-group floating-label-form-group controls">
-                        <label>Bericht</label>
-                        <textarea rows="5" class="form-control" placeholder="Bericht" id="message" required data-validation-required-message="Please enter a message."></textarea>
-                        <p class="help-block text-danger"></p>
-                    </div>
-                </div>
-                <br>
-                <div id="success"></div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary" id="sendMessageButton">Verstuur</button>
-                </div>
-            </form>
+<!--            <form name="sentMessage" method="POST" action="" id="contactForm" novalidate>-->
+<!--                <div class="control-group">-->
+<!--                    <div class="form-group floating-label-form-group controls">-->
+<!--                        <label>Naam</label>-->
+<!--                        <input type="text" class="form-control" placeholder="Naam" id="name" required data-validation-required-message="Please enter your name.">-->
+<!--                        <p class="help-block text-danger"></p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="control-group">-->
+<!--                    <div class="form-group floating-label-form-group controls">-->
+<!--                        <label>Email</label>-->
+<!--                        <input type="email" class="form-control" placeholder="Email" id="email" required data-validation-required-message="Please enter your email address.">-->
+<!--                        <p class="help-block text-danger"></p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="control-group">-->
+<!--                    <div class="form-group col-xs-12 floating-label-form-group controls">-->
+<!--                        <label>Telefoonnummer</label>-->
+<!--                        <input type="tel" class="form-control" placeholder="Telefoonnummer" id="phone" required data-validation-required-message="Please enter your phone number.">-->
+<!--                        <p class="help-block text-danger"></p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="control-group">-->
+<!--                    <div class="form-group floating-label-form-group controls">-->
+<!--                        <label>Bericht</label>-->
+<!--                        <textarea rows="5" class="form-control" placeholder="Bericht" id="message" required data-validation-required-message="Please enter a message."></textarea>-->
+<!--                        <p class="help-block text-danger"></p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <br>-->
+<!--                <div id="success"></div>-->
+<!--                <div class="form-group">-->
+<!--                    <button type="submit" class="btn btn-primary" id="sendMessageButton">Verstuur</button>-->
+<!--                </div>-->
+<!--            </form>-->
         </div>
     </div>
 </div>
